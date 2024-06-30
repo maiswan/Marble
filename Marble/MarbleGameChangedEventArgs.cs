f95c15c1e389e547d81b7fd80171056b3857e158
maiswan <173935047+maiswan@users.noreply.github.com>
2024-06-30 11:00:20 +0800

    Remove ctor in favor of object initializer

--- Marble/MarbleGameChangedEventArgs.cs
+++ Marble/MarbleGameChangedEventArgs.cs
@@ -1,19 +1,12 @@
 ﻿namespace Maiswan.Marble;
 
-internal class MarbleGameChangedEventArgs : EventArgs
+public class MarbleGameChangedEventArgs : EventArgs
 {
-    public bool IsGameOver { get; init; }
+    public required bool IsGameOver { get; init; }
 
-    public int Iteration { get; init; }
+    public required int Iteration { get; init; }
 
-    public IReadOnlyCollection<Team> Teams { get; init; }
+    public required IReadOnlyCollection<Team> Teams { get; init; }
 
     public int TotalPopulation => Teams.Sum(x => x.Population);
-
-    public MarbleGameChangedEventArgs(bool isGameOver, int iteration, IReadOnlyCollection<Team> teams)
-    {
-        IsGameOver = isGameOver;
-        Iteration = iteration;
-        Teams = teams;
-    }
 }
