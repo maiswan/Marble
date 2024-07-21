1ff80740b7664194d03dae14e2b5df0cca1d26ea
maiswan <173935047+maiswan@users.noreply.github.com>
2024-07-21 16:52:11 +0800

    Remove copied file from Marble core

--- Marble/Model/Team.cs
+++ Marble/Model/Team.cs
@@ -4,23 +4,9 @@ namespace Maiswan.Marble;
 
 public readonly record struct Team
 {
-    public string Name { get; init; } = "";
-
-    [JsonConverter(typeof(ConsoleColorConverter))]
-    public ConsoleColor Color { get; init; }
+    public required int Id { get; init; }
 
     public int PreviousPopulation { get; init; }
 
-    public int Population { get; init;  }
-
-    public Team()
-    { }
-
-    // public TeamStatus(Team team)
-    // {
-    //     Name = team.Name;
-    //     Color = team.Color;
-    //     PreviousPopulation = team.PreviousPopulation;
-    //     Population = team.Population;
-    // }
+    public required int Population { get; init; }
 }
