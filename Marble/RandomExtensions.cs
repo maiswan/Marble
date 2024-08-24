a5b64c158bc889775980b7ec518cb81ea336ecf0
maiswan <173935047+maiswan@users.noreply.github.com>
2024-08-24 17:59:11 +0800

    Introduce NextDoubles and NextInts random extension methods

--- Marble/RandomExtensions.cs
+++ Marble/RandomExtensions.cs
@@ -8,4 +8,25 @@ internal static class RandomExtensions
 
         return random.NextDouble() * (max - min) + min;
     }
+
+    internal static IEnumerable<double> NextDoubles(this Random random, double mean, double stdev, int count)
+    {
+        ArgumentOutOfRangeException.ThrowIfLessThan(stdev, 0);
+
+        double[] rates = new double[count];
+        for (int i = 0; i < count; i++)
+        {
+            rates[i] = random.NextDouble();
+        }
+        double originalMean = rates.Average();
+        double originalStdev = Math.Sqrt(rates.Average(x => Math.Pow(x - originalMean, 2)));
+
+        return rates.Select(x => stdev * (x - originalMean) / originalStdev + mean);
+    }
+
+    internal static IEnumerable<int> NextInts(this Random random, double mean, double stdev, int count)
+    {
+        IEnumerable<double> rates = random.NextDoubles(mean, stdev, count);
+        return rates.Select(x => (int)x);
+    }
 }
